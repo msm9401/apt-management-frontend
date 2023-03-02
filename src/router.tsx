9cb75a6f55d47b1b5be5dd233dadde3320b9d5e8
munseungmin <anstmdals94@naver.com>
2023-03-03 06:17:05 +0900

    Not Found page

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
 import Home from "./routes/Home";
+import NotFound from "./routes/NotFound";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
+        errorElement: <NotFound />,
         children: [
             {
               path: "",
